1793d73706d1cc4a6bb7e941705b3e5e625109b7
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-06-15 12:57:46 -0300

    chore: configured path mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					root: ['./src'],
+					alias: {
+						'@dtos': './src/dtos',
+						'@assets': './src/assets',
+						'@components': './src/components',
+						'@screens': './src/screens',
+						'@utils': './src/utils',
+						'@services': './src/services',
+						'@hooks': './src/hooks',
+					},
+				},
+			],
+		],
+	}
+}
